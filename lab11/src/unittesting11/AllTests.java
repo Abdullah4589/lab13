db796078a84b63477294955707cbc5c54126ba3f
Abdullah4589 <150226902+Abdullah4589@users.noreply.github.com>
2024-01-11 15:14:16 +0500

    Add files via upload

---
+++ lab11/src/unittesting11/AllTests.java
@@ -0,0 +1,10 @@
+package unittesting11;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+@RunWith(Suite.class)
+@SuiteClasses({odd.class,even.class})
+public class AllTests {
+
+}
